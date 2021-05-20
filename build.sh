2006a92da060ef2b9917dfa8249ae851af7246cb
scherepanov <sergey@cherepanov.org.ua>
2021-05-21 00:17:15 +0400

    armv6

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd $(dirname $0)
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t digitalspacestudio/debian:buster --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t digitalspacestudio/debian:buster --push .
