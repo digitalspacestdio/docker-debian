d03a0ed3341b366859f79baa33960c63b1eac1c3
S.Cherepanov <sergey@cherepanov.org.ua>
2021-12-25 21:03:42 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname $0)
 
-docker buildx build --platform ${1-'linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6'} -t digitalspacestudio/ruby:2.6-buster --push .
+docker buildx build --platform ${1-'linux/amd64,linux/arm64,linux/arm/v7'} -t digitalspacestudio/ruby:2.6-buster --push .
